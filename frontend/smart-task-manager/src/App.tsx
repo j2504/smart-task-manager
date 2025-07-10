74bec4c370d61b107275161262949e1bfb7fd005
Jerry <jerryjr0425@gmail.com>
2025-07-09 21:38:25 -0500

    feat: connect React frontend to Spring Boot backend via Axios

--- frontend/smart-task-manager/src/App.tsx
+++ frontend/smart-task-manager/src/App.tsx
@@ -1,7 +1,9 @@
-import { useState } from 'react'
-import TaskForm from './components/TaskForm.tsx'
-import TaskList from './components/TaskList.tsx'
+import { useEffect, useState } from 'react';
+import TaskForm from './components/TaskForm.tsx';
+import TaskList from './components/TaskList.tsx';
 import type { Task } from './types/Task.ts';
+import * as taskService from './services/taskService.ts';
+
 
 /**
  * App component - top-level component managing the task list state
@@ -10,37 +12,43 @@ function App() {
   //App-level state for storing all tasks
   const [tasks, setTasks] = useState<Task[]>([]);
 
+  //Fetch tasks from backend on first load
+  useEffect(() => {
+    taskService.getTasks().then(setTasks);
+
+  }, []);
+
   /**
   *Handles adding a new task to the list
   * @param title - The title of the task
   */
-  const addTask = (title: string) => {
-    const newTask: Task = {
-      id: Date.now(),
-      title,
-      status: 'pending'
-    };
-    setTasks([newTask, ...tasks]);
-
+  const addTask = async (title: string) => {
+    const newTask = await taskService.createTask(title);
+    setTasks((prev) => [newTask, ...prev]);
   };
 
-  const updateTaskStatus = (taskId: number, newStatus: Task['status']) => {
-    setTasks(prev =>
-      prev.map(task =>
-        task.id === taskId ? { ...task, status: newStatus } : task)
-    )
+  /**
+   * Handles updating a task status 
+   * @param taskId 
+   * @param newStatus 
+   */
+  const updateTaskStatus = async (id: number, status: Task['status']) => {
+    const updateTask = await taskService.updateTaskStatus(id, status);
+    setTasks((prev) =>
+      prev.map((task) =>
+        (task.id === id ? updateTask : task))
+    );
   }
 
-  const deleteTask = (taskId: number) => {
-    setTasks(prev => prev.filter(task => task.id !== taskId));
+  const deleteTask = async (id: number) => {
+    await taskService.deleteTask(id);
+    setTasks((prev) => prev.filter(task => task.id !== id));
   }
 
   return (
     <div className='container py-5'>
-      <header className='mb-4 text-center'>
-        <h1 className='fw-bold'>Smart Task Manager</h1>
-        <p className='text-muted'>Track your pending, in-progress, and completed tasks</p>
-      </header>
+      <h1 className='text-center fw-bold mb-4'>Smart Task Manager</h1>
+      <p className='text-muted'>Track your pending, in-progress, and completed tasks</p>
       {/* Task input form */}
       <TaskForm onAddTask={addTask} />
       {/* Display list of the tasks */}
